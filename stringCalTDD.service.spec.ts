6deb392f0a100c2574c95eff559fe65a3622c7f8
vishalvishwakarma21 <54456720+vishalvishwakarma21@users.noreply.github.com>
2024-09-30 16:20:53 +0530

    Update stringCalTDD.service.spec.ts

--- stringCalTDD.service.spec.ts
+++ stringCalTDD.service.spec.ts
@@ -1,1 +1,21 @@
 // test driven development for the Angular String Calculator //
+
+import {TestBed} from '@angular/core/testing';
+import {stringCalculatorServices} from './ string-calculator.service';
+
+describe('stringCalculatorServices'),() =>{
+  let testDriven: stringCalculatorServices;
+
+  before(()=>{
+    TestBed.configureTestingModule({});
+    testDriven = TestBed.inject(StringCalculatorService);
+  })
+
+  it('retun 0 from empty string', ()=>{
+    expect(testDriven.add('')).toBe(0);
+  });
+
+    it('retun number when number is given', ()=>{
+    expect(testDriven.add('1')).toBe(1);
+  });
+}
