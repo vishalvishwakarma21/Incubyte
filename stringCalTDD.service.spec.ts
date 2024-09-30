257e4fe1344b0fee6ddc4e463722eac46afa688c
vishalvishwakarma21 <54456720+vishalvishwakarma21@users.noreply.github.com>
2024-09-30 16:34:04 +0530

    Update stringCalTDD.service.spec.ts

--- stringCalTDD.service.spec.ts
+++ stringCalTDD.service.spec.ts
@@ -18,4 +18,20 @@ describe('stringCalculatorServices'),() =>{
     it('retun number when number is given', ()=>{
     expect(testDriven.add('1')).toBe(1);
   });
+
+  it('retun sum of two number', ()=>{
+  expect(testDriven.add('1,2')).toBe(3);
+  });
+  
+       it('throw error on negative numbers', ()=>{
+  expect(testDriven.add('1,-2,-4')).toThrowError('not allowed negative numbers');
+  });
+  
+       it('handling delimiters', ()=>{
+  expect(testDriven.add('1\n3,3')).toBe(7);
+  });
+  
+       it('multiple amount of number', ()=>{
+  expect(testDriven.add('1,2,3,4,5,6,7,8')).toBe(36);
+  });
 }
